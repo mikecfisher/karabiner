70629c2fe45225ff7da9e5094977d6200a36c813
Mike Fisher <mikefisher84@gmail.com>
2025-03-24 23:05:04 -0400

    capslock to esc if alone

--- utils/keyboard-mappings.ts
+++ utils/keyboard-mappings.ts
@@ -6,10 +6,10 @@ export const capsLockToHyper = () =>
   rule('Caps Lock → Hyper').manipulators([map('caps_lock').toHyper().toIfAlone('caps_lock')]);
 
 export const capsLockToControl = () =>
-  rule('Caps Lock → Control').manipulators([
+  rule('Caps Lock → Control/Escape').manipulators([
     // Use Hyper in Lexicon
-    map('caps_lock').toHyper().toIfAlone('caps_lock').condition(ifApp(APP_REGEXES.LEXICON)),
+    map('caps_lock').toHyper().toIfAlone('escape').condition(ifApp(APP_REGEXES.LEXICON)),
 
-    // Use Control everywhere else
-    map('caps_lock').to('left_control').toIfAlone('caps_lock').condition(ifApp(APP_REGEXES.LEXICON).unless()),
+    // Use Control everywhere else with Escape on tap
+    map('caps_lock').to('left_control').toIfAlone('escape').condition(ifApp(APP_REGEXES.LEXICON).unless()),
   ]);
